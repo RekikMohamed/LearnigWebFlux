deeeb6aa0b60583c1cb539e1bfb298a23c54d78a
Mohamed <mohamed.rekik.eni@gmail.com>
2025-04-13 17:47:26 +0200

    Initialize spring-boot project.

---
+++ src/main/java/StartingProject/StartingProject/StartingProjectApplication.java
@@ -0,0 +1,13 @@
+package StartingProject.StartingProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartingProjectApplication.class, args);
+	}
+
+}
